96f83d0de6a444dde32a978672e3396f928708b0
caleb <caleb7947@gmail.com>
2022-04-17 22:33:33 -0400

    adding ManyToManyField to conversation table

--- server/messenger_backend/models/conversation.py
+++ server/messenger_backend/models/conversation.py
@@ -6,12 +6,11 @@ from .user import User
 
 
 class Conversation(utils.CustomModel):
-    # user1 = models.ForeignKey(
-    #     User, on_delete=models.CASCADE, db_column="user1Id", related_name="+"
-    # )
-    # user2 = models.ForeignKey(
-    #     User, on_delete=models.CASCADE, db_column="user2Id", related_name="+", 
-    # )
+    users = models.ManyToManyField(
+        User,
+        through='ConversationUser',
+        related_name='conversations'
+    )
     createdAt = models.DateTimeField(auto_now_add=True, db_index=True)
     updatedAt = models.DateTimeField(auto_now=True)
 
